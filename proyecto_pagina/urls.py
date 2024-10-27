944c9767016de990996e9c343ec78141625a926c
gerald <geraldacunasoftware@gmail.com>
2024-10-27 13:09:59 -0600

    Configuracion de templates html

--- proyecto_pagina/urls.py
+++ proyecto_pagina/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+from app.views import index
 from .settings import MEDIA_ROOT,MEDIA_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index,name='index'),
 ]
 
 urlpatterns += static(MEDIA_URL,document_root=MEDIA_ROOT)
