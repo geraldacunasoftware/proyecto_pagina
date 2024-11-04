cebbfc7988a629ac1455bb871906eb7590dc6928
gerald <geraldacunasoftware@gmail.com>
2024-11-03 20:00:23 -0600

    Debug False

--- proyecto_pagina/settings.py
+++ proyecto_pagina/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y6^zeo$-m$ku5z9m2v2+zzsc9i+ti*qtgi0esw9t_tk*yh2g6)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',]
 
