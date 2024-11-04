d53453e8518656fedf5cb3bb8ee745b7f2bb3a35
gerald <geraldacunasoftware@gmail.com>
2024-11-03 20:06:50 -0600

    python-decouple

--- app/models.py
+++ app/models.py
@@ -5,3 +5,7 @@ class services(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=600)
     image = models.ImageField(upload_to='mediaServices')
+
+class galery(models.Model):
+    service = models.ForeignKey(services,on_delete=models.CASCADE)
+    imgs = models.ImageField(upload_to='mediaGalery')
